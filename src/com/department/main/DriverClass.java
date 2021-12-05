4152c56d7ba73ff6c30eb51faa57fbfe78910ddb
Sravan Kumar Reddy Bontha <bonthasravan@gmail.com>
2021-12-05 16:42:32 +0530

    Update DriverClass.java

--- src/com/department/main/DriverClass.java
+++ src/com/department/main/DriverClass.java
@@ -15,14 +15,14 @@ public class DriverClass {
 		System.out.println(adminDepartment.isTodayAHoliday());
 		System.out.println();
 		
-    	System.out.println("Welcome to " + hrDepartment.departmentName());
+    		System.out.println("Welcome to " + hrDepartment.departmentName());
 		System.out.println(hrDepartment.doActivity());
 		System.out.println(hrDepartment.getTodaysWork());
 		System.out.println(hrDepartment.getWorkDeadline());
 		System.out.println(hrDepartment.isTodayAHoliday());
 		System.out.println();
 
-        System.out.println("Welcome to " + techDepartment.departmentName());
+        	System.out.println("Welcome to " + techDepartment.departmentName());
 		System.out.println(techDepartment.getTodaysWork());
 		System.out.println(techDepartment.getWorkDeadline());
 		System.out.println(techDepartment.getTechStackInformation());
