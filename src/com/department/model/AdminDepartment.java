32853f8e9b686a9d9e27850311a7115c2358af78
Sravan Kumar Reddy Bontha <bonthasravan@gmail.com>
2021-12-05 16:41:35 +0530

    Commit for department files

---
+++ src/com/department/model/AdminDepartment.java
@@ -0,0 +1,14 @@
+package com.department.model;
+
+public class AdminDepartment extends SuperDepartment {
+	
+	public String departmentName() {
+		return "Admin Department";
+	}	
+	public String getTodaysWork() {
+		return "Complete your documents submission";
+	}	
+	public String getWorkDeadline() {
+		return "Complete by EOD";
+	}
+}
